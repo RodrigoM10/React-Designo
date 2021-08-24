1d533a5c7cdbfcc2632165d5c5ce6948a13d99c6
Rodrigo <rodrigomendozar10@gmail.com>
2021-08-24 18:41:39 -0300

    seccion 1, falta responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import  NavbarRB  from './components/Navbar/TheNav';
+import  NavbarRB  from './components/navbar/TheNav';
+import  Section1  from './components/content/Section-1';
+import { Container } from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div>
       <NavbarRB />
-      
+      <Container>
+        <Section1/>
+      </Container>
+     
     </div>
   );
 }
---
+++ src/components/content/Section-1.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import './Section1.css'
+// import iphone from './iphoneframe.png';
+//fontawesome
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons'
+
+export default function Section1() {
+  return (
+    <section>
+      <Container>
+        <div className="section-1 row container-fluid">
+          <div className="col-12 col-lg-6 d-flex flex-column justify-content-evenly p-5 ">
+            <h1>Award-winning custom designs and digital branding solutions</h1>
+            <span>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more bout our services.</span>
+            <button className="btn btn-white">LEARN MORE</button>
+          </div>
+          <div className="col-12 col-lg-6 iphone-container">
+            <div className="iphone-bg">
+              <div className="iphone">
+                {/* <img src={iphone} alt="" /> */}
+                  <div className="frame-div">
+                  <h1>FRAME</h1>
+                  </div>
+                  <div className="frame-btn">
+                  <button className=" btn-white-squad">
+                  <FontAwesomeIcon icon={faChevronRight} />
+                  </button>
+                  </div> 
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
